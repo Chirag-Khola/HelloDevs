a31f7cc3776a8fb14bbaab0666408728c8899e29
Chirag khola <chiragkhola47@gmail.com>
2024-11-29 14:34:49 +0530

    jwt functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,32 +1,12 @@
-// const express = require("express");
-// const router = express.Router();
-// const { check, validationResult } = require('express-validator');
-
-// // @route GET api/profile
-// // @access public
-// router.post(
-//   '/',
-//   [
-//     check('name').not().isEmpty(),
-//     check('email').isEmail(),
-//     check('password').isLength({ min: 5 })
-//   ],
-//   (req, res) => {
-//     const errors = validationResult(req);
-
-//     if(!errors.isEmpty()){
-//         return res.status(400).json({ errors: errors.array() });
-//     }
-//     // console.log(req.body);
-//     res.send("user route");
-//   }
-// );
-
-
-
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const gravatar = require("gravatar");
+const config = require('config');
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route POST /api/users
 // @access public
@@ -35,16 +15,67 @@ router.post(
   [
     check("name", "Name is required").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
-    check("password", "Password must be at least 5 characters long").isLength({ min: 5 }),
+    check("password", "Password must be at least 5 characters long").isLength({
+      min: 5,
+    }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("User registered successfully!");
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists -> if yes -> return error
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      } 
+        //  get user's gravatar
+        const avatar = gravatar.url(email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        });
+
+        user = new User({
+          name,
+          email,
+          avatar,
+          password,
+        });
+
+        // encrypt password
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+          user:{
+            id : user.id
+          }
+        }
+
+        jwt.sign(
+          payload , 
+          config.get('jwtSecret'),
+          { expiresIn: 360000},
+        (err , token) => {
+          if(err) throw err;
+          res.json({token});
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
