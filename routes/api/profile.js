279295209f95555a6eff1849f920662350bc32c1
Chirag khola <chiragkhola47@gmail.com>
2024-12-26 17:25:23 +0530

    get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,7 @@ const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
-const User = require("../../models/User");
+const User = require("../../models/user");
 
 // @route GET api/profile/me
 // @desc Get current user profile
@@ -101,20 +101,58 @@ router.post(
         return res.json(profile);
       }
 
-
       // create
 
       profile = new Profile(profileFeilds);
 
       await profile.save();
       res.json(profile);
-
-
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
   }
 );
+
+// @route GET api/profile
+// @desc Get all profiles
+// @access public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user id
+// @access public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile){
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+      
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
-const config = require('config');
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
-const User = require("../../models/User");
+const User = require("../../models/user");
 
 // @route POST /api/users
 // @access public
@@ -33,43 +33,45 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ errors: [{ msg: "User already exists" }] });
-      } 
-        //  get user's gravatar
-        const avatar = gravatar.url(email, {
-          s: "200",
-          r: "pg",
-          d: "mm",
-        });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      //  get user's gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
 
-        user = new User({
-          name,
-          email,
-          avatar,
-          password,
-        });
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-        // encrypt password
+      // encrypt password
 
-        const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(10);
 
-        user.password = await bcrypt.hash(password, salt);
+      user.password = await bcrypt.hash(password, salt);
 
-        await user.save();
+      await user.save();
 
-        const payload = {
-          user:{
-            id : user.id
-          }
-        }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-        jwt.sign(
-          payload , 
-          config.get('jwtSecret'),
-          { expiresIn: 360000},
-        (err , token) => {
-          if(err) throw err;
-          res.json({token});
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
       );
     } catch (err) {
