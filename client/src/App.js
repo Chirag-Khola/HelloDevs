b890299ae01a059aac9c5c41263d73f1cf12b068
Chirag khola <chiragkhola47@gmail.com>
2025-01-12 00:01:02 +0530

    react routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
-import React , { Fragment } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Landing from './components/layout/Landing'
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Landing from "./components/layout/Landing";
 
-function App  () {
+function App() {
   return (
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route
+            path="/register"
+            element={
+              <section className="container">
+                <Register />
+              </section>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <section className="container">
+                <Login />
+              </section>
+            }
+          />
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
